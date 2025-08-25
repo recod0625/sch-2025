825f7e5c362ee27eab88169c22e019af8d2d33fd
reccod0625 <recod0625@gmail.com>
2025-08-25 11:47:37 +0900

    2025-08-25 commit~

---
+++ sprintboot/src/test/java/com/sch/sprintboot/SprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.sprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
